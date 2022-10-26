119705242deac21059dad9c48bbd821288fc0b52
SerdarHELLI <62758782+SerdarHelli@users.noreply.github.com>
2022-10-26 21:32:58 +0300

    Create pip-dependencies.sh

---
+++ pip-dependencies.sh
@@ -0,0 +1,22 @@
+#!/bin/sh
+. $PREFIX/etc/profile.d/conda.sh  # do not edit
+conda activate $PREFIX            # do not edit
+
+# add your pip packages here; bottle is just an example!
+# replace it with your dependencies
+python -m pip install bottle
+
+python -m pip install  albumentations==0.4.3
+python -m pip install  opencv-python==4.1.2.30
+python -m pip install pudb==2019.2
+python -m pip install  imageio==2.9.0
+python -m pip install  imageio-ffmpeg==0.4.2
+python -m pip install  pytorch-lightning==1.4.2
+python -m pip install  omegaconf==2.1.1
+python -m pip install  test-tube>=0.7.5
+python -m pip install  streamlit>=0.73.1
+python -m pip install  einops==0.3.0
+python -m pip install  torch-fidelity==0.3.0
+python -m pip install  transformers==4.3.1
+python -m pip install  -e git+https://github.com/CompVis/taming-transformers.git@master#egg=taming-transformers
+python -m pip install  -e git+https://github.com/openai/CLIP.git@main#egg=clip
